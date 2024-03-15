881d1b1dee759db69313d9c08567cc603d713e9f
Shantanu Joshi <shjoshi@ieee.org>
2024-03-14 23:06:14 -0700

    test data in testdata.npy. Return a heap allocated array (gamma) by casting it to ndarray<nb::numpy>

--- src/dpmatchsrvf/pydpmatchsrvf.cpp
+++ src/dpmatchsrvf/pydpmatchsrvf.cpp
@@ -1,13 +1,19 @@
 #include "dpmatch.h"
 #include <nanobind/nanobind.h>
 #include <nanobind/ndarray.h>
-#include <nanobind/stl/vector.h>
 
+#include<iostream>
+using namespace std;
 namespace nb = nanobind;
 using nparray = nb::ndarray<nb::numpy, float, nb::ndim<2>>;
 
+
+float data[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+
 auto match(const nparray &q1, const nparray &q2)
 {
+
     dpmatch dpmatchobj;
     size_t shape[2] = {q1.shape(0), q1.shape(1)};
     int n = q1.shape(0);
@@ -18,13 +24,13 @@ auto match(const nparray &q1, const nparray &q2)
 
     float *gamma = nullptr;
     gamma = dpmatchobj.match(n, T, q1ptr, q2ptr);
-    std::vector<float> gamma_vect {gamma, gamma + T};
-    delete gamma;
-    return(gamma_vect);
+    return (nb::ndarray<nb::numpy, float>(gamma, {(unsigned long) T}));
 }
 
 NB_MODULE(dpmatchsrvf, m)
 {
     m.doc() = "nanobind dpmatchsrvf plugin";
-    m.def("match", &match, nb::rv_policy::move);
+    m.def("match", &match, nb::rv_policy::take_ownership);
 }
+
+
